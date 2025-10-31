6c6379ffc898e4fb436f4857370c0c97ece65fc3
JJ039 <jiayi.guo.2024@computing.smu.edu.sg>
2025-10-31 15:09:55 +0000

    debug

--- src/components/subcomponents/BlogPost.vue
+++ src/components/subcomponents/BlogPost.vue
@@ -24,7 +24,6 @@
         <div class="card-body">
             <h5 class="card-title">{{ subject }}</h5>
             <p class="card-text">{{ entry }}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
 </div>
 
